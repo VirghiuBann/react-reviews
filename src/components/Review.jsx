de123b58042c0de7ecf201f092e4e002a92c4769
virghiu <virghiu@gmail.com>
2023-09-28 21:13:43 -0400

    feat: icons installed and rendered

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi2'
+import { FaQuoteRight } from 'react-icons/fa'
 
 const Review = ({ id, name, job, image, text }) => {
   return (
-    <article>
-      <div>
-        <img src={image} alt={name} />
+    <article className='review'>
+      <div className='img-container'>
+        <img className='person-img' src={image} alt={name} />
+        <div className='quote-icon'>
+          <FaQuoteRight />
+        </div>
       </div>
       <div>
         <h2>{name}</h2>
         <h4>{job}</h4>
         <p>{text}</p>
+      </div>
+      <div>
+        <HiChevronLeft />
+        <HiChevronRight />
       </div>
       <button className='btn'>Surprise Me</button>
     </article>
